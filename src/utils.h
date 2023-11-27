19ac34f1606407e5b75a176102260f2bc1927f34
steven <stevenchiffe@gmail.com>
2023-11-27 01:44:31 +0100

    Init of SDL2 window and somes utils functions added (Log)

---
+++ src/utils.h
@@ -0,0 +1,44 @@
+#pragma once
+#include <stdio.h>
+#include <time.h>
+#include <string.h>
+#define DEBUG 1
+
+typedef enum {
+    LOG_INFO,
+    LOG_WARNING,
+    LOG_ERROR
+} LogLevel;
+
+const char* logLevelToString(LogLevel level)
+{
+    switch (level) {
+        case LOG_INFO: return "INFO";
+        case LOG_WARNING: return "WARNING";
+        case LOG_ERROR: return "ERROR";
+        default: return "UNKNOWN";
+    }
+}
+
+void get_formatted_time(char *buffer)
+{
+    time_t rawtime;
+    struct tm * timeinfo;
+    time(&rawtime);
+    timeinfo = localtime(&rawtime);
+    strftime(buffer, 80, "%Y-%m-%d %H:%M:%S", timeinfo);
+}
+
+#ifdef DEBUG
+    #define LOG(TYPE, MESSAGE) do { \
+        char time_buffer[80]; \
+        get_formatted_time(time_buffer); \
+        printf("[%s] [%s] %s:%d %s(): %s\n", logLevelToString(TYPE), time_buffer, __FILE__, __LINE__, __func__, MESSAGE); \
+    } while(0)
+#else
+    #define LOG(TYPE, MESSAGE) do { \
+        char time_buffer[80]; \
+        get_formatted_time(time_buffer); \
+        printf("[%s] [%s] : %s\n", logLevelToString(TYPE), time_buffer, MESSAGE); \
+    } while(0)
+#endif
